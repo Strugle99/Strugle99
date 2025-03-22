f496aebb9142279447f86f9dc81b47fd827fa47e
rooney_mark <154583377+Strugle99@users.noreply.github.com>
2025-03-22 11:19:22 -0700

    Add files via upload
    this is the program i made first after being embarrassed by my friends of which they are so resourceful

---
+++ boot2.py
@@ -0,0 +1,104 @@
+name=input('hello\n')
+print()
+print()
+
+name1 = input('Am Mac Osz, what is your name?\n')
+print('Hello, %s!' % name1)
+print()
+print()
+
+name2=input('Where do you live?\n')
+print( 'I have a freind their, %s!' % name1)
+print()
+print()
+
+question1=input('Can you became my freind?\n')
+print('its okay, %s!' % name1)
+print()
+print()
+
+ans1=input('What is your favourite dish?\n')
+print('Its delicious, %s!' % ans1)
+print()
+print()
+
+ans2=input('What is your worst dish?\n')
+print('Ohhhh!, %s!' %name1)
+print()
+print()
+ask=input("Why don't you like it?\n")
+print("That's the reason why you don't like, %s!" %ans2)
+print()
+print()
+
+question4=input('Tell me if you know Eroun Musk?\n')
+print("Ohh thats all, %s!" %name1)
+print()
+print()
+
+ans=input("Who is the world's richest man?\n")
+print('Ohh hahaha, %s!' %question4)
+print()
+print()
+
+ans=input('tell me about your school?\n')
+print('Ohh hohoho, %s!' %name1*2)
+print()
+print()
+
+ans2=input('tell me the person you were with at school?\n')
+print('Ohh, %s!' %ans2)
+print()
+print()
+
+ans3=input('tell me more about your freind?\n')
+print('ohh he seems to love u so much, %s!' %ans2)
+print()
+
+print()
+
+ans35=input('Do you have a girl freind?\n')
+print('Ohh how prety she is, %s!' %name1+ans35)
+print()
+print()
+
+ans45=input("Ididn't understand your freinds?\n")
+print('ok, %s!' %name1+ans35+ans3+ans2)
+print()
+print()
+
+ans4=input('ohh let us talk about Project and tjen Church?\n')
+print('let it be now, %s!' %ans4)
+print()
+print()
+
+ans5=input('ohh tell me your project number?\n')
+print()
+print()
+
+ans6=input('what is your email?\n')
+print('ok thats good, %s!' %ans5+name1)
+print()
+print()
+
+question=input('nice to meet you, %s!' %name1)
+print('my dear , %s!' %name1*2)
+print()
+print()
+
+name20=input('loved your conversations?\n')
+print('Good bye, %s!' %name1*3)
+print()
+print()
+
+print('CREATED BY RONY SEBULIBA')
+print()
+
+print('IN CONTACT  with')
+print('Rooney THTz software')
+print()
+
+print('I thank God for what he has done ')
+print()
+print()
+
